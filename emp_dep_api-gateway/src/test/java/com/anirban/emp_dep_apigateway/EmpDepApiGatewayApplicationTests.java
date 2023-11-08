e7f433badf77f981fb2527509bb2ecd66a59cb3a
Anirban <ad653157@gmail.com>
2023-11-08 19:14:25 +0530

    Added a functional api gateway service

---
+++ emp_dep_api-gateway/src/test/java/com/anirban/emp_dep_apigateway/EmpDepApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirban.emp_dep_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpDepApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
